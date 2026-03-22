a4e0912d7335453bc30baf736241298b5e8f9f9b
Asakala Geraghty <asakala@Asakalas-MBP.lan>
2026-03-22 11:09:50 +0000

    New minimal styling

--- app/page.tsx
+++ app/page.tsx
@@ -206,12 +206,12 @@ export default function AIPortfolio() {
 <div className="mb-28">
   <div className="grid grid-cols-12 gap-8">
   <div className="col-start-5 col-span-8">
-    <h2 className="text-4xl font-bold text-slate-100 mb-4">See how my experience matches</h2>
+    <h2 className="text-4xl font-bold text-slate-100 mb-4">See how my experience matches your project</h2>
   
     <div className="govuk-form-group">
       <label htmlFor="job-description" className="block mb-2">
         <span className="text-slate-300 text-lg font-medium leading-relaxed block">
-          Paste a job description to see my most relevant projects
+          Paste a job description or list of skills
         </span>
       </label>
       
